ff371e587e3267cb3fc3f696c8754658263e4ed7
Chloe <page.prologue@gmail.com>
2024-07-16 23:41:49 +0900

    feat: project init

---
+++ src/test/kotlin/com/anchoreer/ChatServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anchoreer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
